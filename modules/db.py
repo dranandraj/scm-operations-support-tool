ef0be2e6e2eb6e16cecf7c42fb7793c5889ff4b6
Anand Raj <drarerjp@gmail.com>
2026-08-22 18:14:50 +0000

    Fix PostgreSQL connection port

--- modules/db.py
+++ modules/db.py
@@ -3,7 +3,7 @@ import psycopg2
 def get_connection():
     return psycopg2.connect(
         host="localhost",
-        port="5432",
+        port="5433",
         database="scm_support_db",
         user="postgres",
         password="6978"
