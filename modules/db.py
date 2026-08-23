e66b8e5d652f66481e0c9a5c95d0625131e7ed2e
Anand Raj <drarerjp@gmail.com>
2026-08-23 20:11:19 +0900

    A Secure database configuration# Conflicts:

--- modules/db.py
+++ modules/db.py
@@ -1,10 +1,8 @@
 import psycopg2
+from config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
+
 
 def get_connection():
     return psycopg2.connect(
-        host="localhost",
-        port="5433",
-        database="scm_support_db",
-        user="postgres",
-        password="6978"
+        host=DB_HOST, port=DB_PORT, database=DB_NAME, user=DB_USER, password=DB_PASSWORD
     )
--- test_connection.py
+++ test_connection.py
@@ -1,15 +1,12 @@
 import psycopg2
+from config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
 
 try:
     connection = psycopg2.connect(
-        host="localhost",
-        port="5432",
-        database="scm_support_db",
-        user="postgres"
+        host=DB_HOST, port=DB_PORT, database=DB_NAME, user=DB_USER, password=DB_PASSWORD
     )
 
     print("Database Connected Successfully!")
-
     connection.close()
 
 except Exception as error:
